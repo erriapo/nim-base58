b8d3b0d6925a68747b17c8a4cefedcdf99380a43
Gavin <rubycoder@gmail.com>
2018-10-28 20:55:43 -0700

    Completed base16 version

--- src/base58.nim
+++ src/base58.nim
@@ -1,5 +1,6 @@
 # base58
 # Copyright Gavin Chun Jin
+# Reference https://rosettacode.org/wiki/Base58Check_encoding
 # Base58 check encoder and decoder
 
 import bigints
@@ -30,19 +31,33 @@ proc toBase58(s: string, radix: Radix = Base16): string =
           # echo r, "  ", r.type.name, parseInt(r.toString(10))
           result &= alphabet[parseInt(r.toString(10))]
           x = x.div(big58)
-          
-      result = reverse(result)
   else:
-      # TODO for base16 hashes
-      # REF https://rosettacode.org/wiki/Base58Check_encoding
-      result = "TODO" 
+      var t = s
+      if startsWith(s, "0x"):
+          t = s[2 .. ^1]
+      var x = initBigInt(t, 16)
+      while x > big0: 
+          var r = x.mod(big58)
+          result &= alphabet[parseInt(r.toString(10))]
+          x = x.div(big58)
+      
+  result = reverse(result)
 
 when isMainModule:
   import unittest
   suite "test encodings":
-    test "base10":
+    test "base10 #1":
       check:
         toBase58("25420294593250030202636073700053352635053786165627414518", Base10) == "6UwLL9Risc3QfPqBUvKofHmBQ7wMtjvM"
-    test "base16":
+
+    test "base16 #1":
+      check:
+        toBase58("800C28FCA386C7A227600B2FE50B7CAE11EC86D3BF1FBE471BE89827E19D72AA1D507A5B8D", Base16) == "5HueCGU8rMjxEXxiPuD5BDku4MkFqeZyd4dZ1jvhTVqvbTLvyTJ"
+
+    test "base16 #2":
       check:
         toBase58("0x73696d706c792061206c6f6e6720737472696e67", Base16) == "2cFupjhnEsSn59qHXstmK2ffpLv2"
+
+    test "base16 #3":
+      check:
+        toBase58("0x10c8511e", Base16) == "Rt5zm"
